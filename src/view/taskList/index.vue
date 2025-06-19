02822ecea44d041914926f11a9f25b9ae8380d52
lin <943979843@qq.com>
2025-06-20 00:12:39 +0800

    'feat:完成流式过滤输出内容'

--- src/view/taskList/index.vue
+++ src/view/taskList/index.vue
@@ -1,6 +1,21 @@
 <template>
     <h2 @click="start">任务队列</h2>
-    <section>{{ str }}</section>
+    <section>
+        <div v-for="item, index in streamJson" :key="index + 'stream'">
+            <h2>板书内容：</h2>
+            <div style="color: aliceblue;">
+                {{ item.板书 }}
+            </div>
+            <div v-for="gaoitem, inx in item.讲稿" :key="index + inx + 'jiangao'" style="background-color: aquamarine;">
+                <h2 v-if="gaoitem">讲解:</h2>
+                {{gaoitem}}
+            </div>
+            <div v-if="item.提问" style="background-color: bisque;">
+                <h2>提问：</h2>
+                {{ item.提问 }}
+            </div>
+        </div>
+    </section>
 </template>
 <script setup lang="ts">
 import { ref } from "vue"
@@ -11,8 +26,8 @@ const print = new lds.Print()
 const print2 = new lds.Print()
 function start() {
     let switchs = true
-    let cacheArr:string[] = []
-    let banshuArr:string[] = []
+    let cacheArr: string[] = []
+    let banshuArr: string[] = []
     let cache = {
         开场: '',
         板书: '',
@@ -28,46 +43,49 @@ function start() {
 
     print.printChars({
         animte: true,
-        second: 169, // 多少秒输出完整个句子
+        second: 50, // 多少秒输出完整个句子
         text: streamInfo,
         callback: (result: string) => {
             getLabelConent(result)
         }
     })
 
-   setInterval(() => {
-    console.log("===============666-", banshuArr)
-   }, 1000);
-
     async function printstring() {
-        const fenduanStr = cache[index]
-        await printbanshu(fenduanStr)
-        index++
-        printstring()
+        const fenduanStr = streamJson[bookIndex]
+        const { 板书 } = fenduanStr
+        // const res =  await printbanshu(板书)
+        // str.value = res
+        // index++
+        // printstring()
     }
+    setTimeout(()=>{
+        printstring()
+    }, 1000)
+    
 }
 
 let findSwitch = false // 排查开关
 let theLabel = '' // 当前的label
 let bookConten = '' // 板书内容
 let speakConten = '' // 讲稿内容
-let _bookContens:string[] = [] // 板书内容
-let _speakContens:string[] = [] // 讲稿内容
+let _bookContens: string[] = [] // 板书内容
+let _speakContens: string[] = [] // 讲稿内容
 
 let bookIndex = 0 // 板书indx
 let speakIndex = 0 // 讲稿indx
 
-const streamJson: any = [] // 总数组
+const streamJson = ref<any>([]) // 总数组
 
 function getLabelConent(streamtext: string) { // 问题1：假设板书内容比较长，遇到下一个[才去push的话，就没有办法做到时效性的流式输出内容
     console.log("当前的内容:板书：", streamJson)
-     
-        if (streamtext === "[") { // 1 排查是否有括号
-            if(bookConten) {
-            bookIndex ++
+
+    if (streamtext === "[") { // 1 排查是否有括号
+        if (bookConten) {
+            bookIndex++
             speakIndex = 0
+            _speakContens = []
         }
-        if(speakConten) {
+        if (speakConten) {
             speakIndex++
         }
         findSwitch = true
@@ -75,22 +93,26 @@ function getLabelConent(streamtext: string) { // 问题1：假设板书内容比
         bookConten = ''
         speakConten = ''
     }
-    if(theLabel.length === 4 ) {
-        switch(theLabel) {
+    if (theLabel.length === 4) {
+        switch (theLabel) {
             case "[板书]":
-            bookConten += streamtext
-            streamJson[bookIndex] = {
-                "板书": bookConten,
-                "讲稿": []
-            }
-            break;
+                bookConten += streamtext
+                streamJson.value[bookIndex] = {
+                    "板书": bookConten,
+                    "讲稿": [],
+                    "提问": ''
+                }
+                break;
             case "[讲稿]":
-            speakConten += streamtext
-            _speakContens[speakIndex] = speakConten
-            streamJson[bookIndex-1]["讲稿"] = [
-                ..._speakContens
-            ]
-            break;
+                speakConten += streamtext
+                _speakContens[speakIndex] = speakConten
+                streamJson.value[bookIndex - 1]["讲稿"] = [
+                    ..._speakContens
+                ]
+                break;
+            case "[提问]":
+                streamJson.value[bookIndex - 1]["提问"] += streamtext
+                break;
         }
         return
     }
@@ -98,30 +120,23 @@ function getLabelConent(streamtext: string) { // 问题1：假设板书内容比
         theLabel += streamtext
     }
 }
-
-
-
-
-function poxys() {
-
-}
-
-
 function printbanshu(text) {
     let strings = ''
     return new Promise((resolve) => {
-        print2.printChars({
-            animte: true,
-            second: 0.5, // 多少秒输出完整个句子
-            text: text,
-            callback: (result: string) => {
-                strings += result
-                str.value += result
-                if (strings.length === text.length) {
-                    resolve("==============" + text + '播放完了')
-                }
-            }
-        })
+        resolve(text)
+        // print2.printChars({
+        //     animte: true,
+        //     second: 0.5, // 多少秒输出完整个句子
+        //     text: text,
+        //     callback: (result: string) => {
+        //         strings += result
+        //         str.value += result
+                
+        //         if (strings.length === text.length) {
+        //             resolve("==============" + text + '播放完了')
+        //         }
+        //     }
+        // })
     })
 }
 
